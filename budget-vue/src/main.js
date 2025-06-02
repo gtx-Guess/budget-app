3537f5d7f6206927e8640304eb58719b895f0cf2
gtx-Guess <tigranalpha@gmail.com>
2025-06-01 18:37:12 -0700

    Adjusting interceptor logic, fixing bug for infinite polling on auth endpoints

--- budget-vue/src/main.js
+++ budget-vue/src/main.js
@@ -6,26 +6,30 @@ import '@/styles.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 axios.interceptors.response.use(
-  response => response,
-  async error => {
-    if (error.response?.status === 401) {
-      console.log('🔄 Attempting token refresh...')
-      try {
-        await axios.post('/api/refresh_token')
-        console.log('✅ Refresh successful, retrying...')
-        return axios.request(error.config)
-      } catch (refreshError) {
-        console.log('❌ Refresh failed, redirecting...')
-        router.push('/login')
-      }
+    (response) => response,
+    async (error) => {
+        if (
+            error.response?.status === 401 &&
+            !error.config.url.includes("/api/authenticated") &&
+            !error.config.url.includes("/api/refresh_token")
+        ) {
+            console.log("🔄 Attempting token refresh...");
+            try {
+                await axios.post("/api/refresh_token");
+                console.log("✅ Refresh successful, retrying...");
+                return axios.request(error.config);
+            } catch (refreshError) {
+                console.log("❌ Refresh failed, redirecting...");
+                router.push("/login");
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error)
-  }
-)
+);
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount('#app');
+createApp(App).use(pinia).use(router).mount("#app");
